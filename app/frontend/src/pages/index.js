c85cc642e2ed9a2cd3a5ccf1de076531e27274ff
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-10-12 19:06:08 -0300

    ajusta aqui e ali

--- app/frontend/src/pages/index.js
+++ app/frontend/src/pages/index.js
@@ -9,17 +9,17 @@ export default function Home() {
     return (
       <div className=' flex flex-col w-full h-screen justify-between border p-1'>
         <Header />
-        <div className=" w-full flex items-center justify-center">
-          <div className="flex flex-col bg-blue-100 p-8 rounded-xl shadow-md items-center">
+        <div className=" w-full  flex items-center justify-center">
+          <div className="flex flex-col bg-gray-800 p-8 rounded-xl shadow-md items-center mb-40">
             <h1 className="text-4xl text-center mb-6 text-black bg-white rounded-xl w-full">Bem-vindo</h1>
-            <div className="flex text-2xl text-center mb-4 items-center justify-center bg-black w-min rounded-xl">
+            <div className="flex text-2xl text-center mb-4 items-center justify-center w-min rounded-xl">
               <ShoppingBasket />
             </div>
-            <p className="text-center text-gray-800 mb-4">
+            <p className="text-center text-white mb-4">
               Desafio Técnico - CREDFRANCO
             </p>
             <Link href="/login" className='flex items-center justify-center'>
-              <button className="bg-gray-800 text-white px-4 py-2 rounded-xl">
+              <button className="border border-white text-white px-4 py-2 rounded-xl">
                 Ir para o Login
               </button>
             </Link>
